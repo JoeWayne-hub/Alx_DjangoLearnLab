5e205b10af9f9683dd807d57a2f12c21ef4720e5
joseph aderibigbe <aderibigbejoseph131@gmail.com>
2025-09-07 17:13:33 +0100

    Adding new files for library and list of books

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from relationship_app import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),  # URL for the function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from relationship_app.models import Book
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+from django.views.generic import DetailView
+from relationship_app.models import Library
+
+# Class-based view to display a specific library's details, including its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
