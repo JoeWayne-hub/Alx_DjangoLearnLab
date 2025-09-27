0f5fe064ca87008fb0aa16fd9cc29166ee9af493
joseph aderibigbe <aderibigbejoseph131@gmail.com>
2025-09-27 22:03:05 +0100

    few more changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics, permissions
 from django.utils import timezone
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 # 📖 List all books (anyone can view)
 class BookListView(generics.ListAPIView):
