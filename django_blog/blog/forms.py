5009d58d1213f766b903dfe1fdbbe42771304fdb
joseph aderibigbe <aderibigbejoseph131@gmail.com>
2025-10-05 00:28:11 +0100

    Added tagging and search functionality to blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
-from .models import Post
+from .models import Post, Tag
 from .models import Comment
 
 class CustomUserCreationForm(UserCreationForm):
@@ -34,19 +34,20 @@ class ProfileForm(forms.ModelForm):
 
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(required=False, help_text="Add tags separated by commas")
+
     class Meta:
         model = Post
-        fields = ["title", "content"]  # author set automatically
+        fields = ['title', 'content', 'tags']
 
-class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'rows':3}), max_length=2000)
-
-    class Meta:
-        model = Comment
-        fields = ['content']
-
-    def clean_content(self):
-        data = self.cleaned_data['content'].strip()
-        if not data:
-            raise forms.ValidationError("Comment cannot be empty.")
-        return data
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if commit:
+            instance.save()
+        # handle tags
+        tags_str = self.cleaned_data.get('tags', '')
+        tag_names = [name.strip() for name in tags_str.split(',') if name.strip()]
+        for name in tag_names:
+            tag, created = Tag.objects.get_or_create(name=name)
+            instance.tags.add(tag)
+        return instance
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,12 +8,20 @@ from django.db.models.signals import post_save
 from django.urls import reverse
 from django.conf import settings
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
@@ -45,7 +53,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-    
+
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
