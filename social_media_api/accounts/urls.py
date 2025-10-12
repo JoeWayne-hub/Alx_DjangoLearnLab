9dc025f2a58db1aaf17fd28ae678c9f8db8dc440
joseph aderibigbe <aderibigbejoseph131@gmail.com>
2025-10-12 16:16:07 +0100

    Add follow/unfollow and feed features

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
+from .views import FollowUserView, UnfollowUserView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-     path('follow/<int:user_id>/', views.FollowToggleAPIView.as_view(), name='follow-toggle'),
+    path('follow/<int:user_id>/', views.FollowToggleAPIView.as_view(), name='follow-toggle'),
     path('following/', views.FollowingListView.as_view(), name='following-list'),
     path('followers/', views.FollowersListView.as_view(), name='followers-list'),
-      path('feed/', views.FeedView.as_view(), name='feed'),
+    path('feed/', views.FeedView.as_view(), name='feed'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
+
